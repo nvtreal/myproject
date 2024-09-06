dfabf4bd45f5fa08dc3157b2de5d554261e9cfde
Jonhtruong <vanlocgroups@gmail.com>
2024-09-07 00:15:22 +0700

    Demo Project Maven Modul 4

---
+++ src/test/java/com/edureka/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,32 @@
+package com.edureka.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void m1() {
+		System.out.println("from m1");
+	}
+	
+	@Test
+	public void m2() {
+		System.out.println("from m2");
+	}
+	
+	@Test
+	public void m3() {
+		System.out.println("from m3");
+	}
+	
+	@Test
+	public void m4() {
+		System.out.println("from m4");
+	}
+}
